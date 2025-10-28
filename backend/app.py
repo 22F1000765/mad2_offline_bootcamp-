ae81fc6c3e78d4c1e6650d55d6d7afcea5b91167
Pranitha <pranithamarimuthu251@gmail.com>
2025-10-28 17:56:17 +0530

    flask-cors enabled

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
 ## This is what we were doing in mad1
 # @app.route('/')
@@ -14,7 +16,7 @@ class HelloWorld(Resource):
     def get(self):
         return {'message': 'Hello, World!'}
     
-api.add_resource(HelloWorld, '/')
+api.add_resource(HelloWorld, '/message')
 
 if __name__ == '__main__':
     app.run(debug=True)
