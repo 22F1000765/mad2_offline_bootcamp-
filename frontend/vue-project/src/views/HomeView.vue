772c5dc3e810c9a0dcb5eb485b46dd0fb901c92b
Pranitha <pranithamarimuthu251@gmail.com>
2025-10-31 17:21:18 +0530

    Started with Vue part and made simple about page

---
+++ frontend/vue-project/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+function loadMessage() {
+  fetch('http://localhost:5000/')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      document.getElementById('message').innerText = data.message
+    })
+    .catch((error) => {
+      document.getElementById('message').innerText = 'Error fetching message'
+      console.log(error)
+      console.error('Error fetching message:', error)
+    })
+}
+</script>
+A Da
+<template>
+  <h1>Welcome to MAD2 Project</h1>
+
+  <p id="message">Click below button to load message</p>
+
+  <button @click="loadMessage()">Get message from backend</button>
+</template>
